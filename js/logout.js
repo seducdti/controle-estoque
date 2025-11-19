9848dd13ed5ba4d79ab527e4e4bb2bfdb7ba95d6
seducdti <seducdti9@gmail.com>
2025-11-19 16:06:21 -0300

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,8 +1,8 @@
 import { auth } from "./firebase.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-window.logout = function () {
+export function logout() {
   signOut(auth).then(() => {
     window.location.href = "login.html";
   });
-};
+}
