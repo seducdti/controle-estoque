4971d0f880c1a1667d04d494c295fd32696de734
seducdti <seducdti9@gmail.com>
2025-11-19 16:26:51 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,18 +5,27 @@ import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/1
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const usuario = document.getElementById("usuario").value.trim();
+  const usuario = document.getElementById("usuario").value.trim().toLowerCase();
   const senha = document.getElementById("senha").value.trim();
 
-  // Usuário padrão convertido para e-mail interno
-  const emailConvertido = usuario.toLowerCase() === "admin"
-    ? "adm@sistema.com"
-    : usuario + "@sistema.com";
+  // Converte automaticamente o usuário para o e-mail do Firebase
+  let emailConvertido = usuario + "@sistema.com";
+
+  // Para admin usa o e-mail escondido:
+  if (usuario === "admin") {
+    emailConvertido = "adm@sistema.com";
+  }
 
   try {
     await signInWithEmailAndPassword(auth, emailConvertido, senha);
+
+    // Salva nível
+    const nivel = (usuario === "admin") ? "admin" : "operador";
+    localStorage.setItem("nivel", nivel);
+
     window.location.href = "index.html";
   } catch (error) {
+    console.log(error);
     alert("Usuário ou senha incorretos!");
   }
 });
