eb34d864cb677cec660d80cd421a9892170609f7
seducdti <seducdti9@gmail.com>
2025-11-19 16:11:31 -0300

    Update produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -1,67 +1,41 @@
-function getDados() {
-  return JSON.parse(localStorage.getItem("estoque")) || { produtos: [], entradas: [], saidas: [] };
-}
+// produtos.js
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-function salvarDados(d) {
-  localStorage.setItem("estoque", JSON.stringify(d));
-}
+const form = document.getElementById("formProduto");
+const tabela = document.getElementById("listaProdutos");
 
-function gerarID(dados) {
-  const ultimo = dados.produtos.map(p => p.id);
-  return ultimo.length ? Math.max(...ultimo) + 1 : 1;
-}
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-function atualizarTabela() {
-  const tbody = document.getElementById("listaProdutos");
-  const dados = getDados();
-  tbody.innerHTML = "";
+  const nome = document.getElementById("nomeProduto").value;
+  const quantidade = Number(document.getElementById("quantidadeProduto").value);
 
-  dados.produtos.forEach((p, i) => {
-    tbody.innerHTML += `
+  await addDoc(collection(db, "produtos"), { nome, quantidade });
+
+  alert("Produto cadastrado!");
+  form.reset();
+  carregarProdutos();
+});
+
+async function carregarProdutos() {
+  tabela.innerHTML = "";
+  const snap = await getDocs(collection(db, "produtos"));
+
+  snap.forEach(doc => {
+    const p = doc.data();
+    const tr = `
       <tr>
-        <td>${p.id}</td>
         <td>${p.nome}</td>
         <td>${p.quantidade}</td>
-        <td>
-          <button onclick="editarProduto(${i})">Editar</button>
-          <button onclick="excluirProduto(${i})">Excluir</button>
-        </td>
       </tr>
     `;
+    tabela.innerHTML += tr;
   });
 }
 
-function excluirProduto(i) {
-  const dados = getDados();
-  dados.produtos.splice(i, 1);
-  salvarDados(dados);
-  atualizarTabela();
-}
-
-function editarProduto(i) {
-  const dados = getDados();
-  const p = dados.produtos[i];
-  document.getElementById("nome").value = p.nome;
-  document.getElementById("formProduto").dataset.editando = i;
-}
-
-document.getElementById("formProduto").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const nome = document.getElementById("nome").value;
-  const dados = getDados();
-  const edit = e.target.dataset.editando;
-
-  if (edit !== undefined) {
-    dados.produtos[edit].nome = nome;
-    delete e.target.dataset.editando;
-  } else {
-    dados.produtos.push({ id: gerarID(dados), nome, quantidade: 0 });
-  }
-
-  salvarDados(dados);
-  e.target.reset();
-  atualizarTabela();
-});
-
-atualizarTabela();
+carregarProdutos();
