febfc58c9812c949e75c50ec37a3db27032c3498
seducdti <seducdti9@gmail.com>
2025-11-24 11:47:11 -0300

    Delete js/logout.js

--- js/logout.js
+++
@@ -1,9 +0,0 @@
-// logout.js
-import { auth } from "./firebase.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-
-window.logout = function () {
-  signOut(auth).then(() => {
-    window.location.href = "login.html";
-  });
-};
